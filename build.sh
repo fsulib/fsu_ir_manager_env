dcb296574685cffb1d1febe616167dc091f11528
Bryan J. Brown <bjbrown@fsu.edu>
2018-10-04 11:14:23 -0400

    Adds PHP to installation list

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ apt-get dist-upgrade -y
 apt-get autoremove -y
 apt-get autoclean -y
 
+# Download dependencies
+apt install php7.2-cli
+
 # Download IR manager tools
 cd /home/vagrant
 git clone https://github.com/fsulib/pmc_grabber
+chown -R vagrant /home/vagrant/pmc_grabber
